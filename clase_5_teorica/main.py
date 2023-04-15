fa967a37d94c6b8a2d221a1f09590e9d7c3fdbff
franpuricelli <francopurib@gmail.com>
2023-04-15 14:09:19 -0300

    clase 5 teorica y tarea

---
+++ clase_5_teorica/main.py
@@ -0,0 +1,13 @@
+#tarea: hacer un metodo para saber si las golondrinas y los dragones estan felices (golondrias si tienen mas de 50 d energia y 
+#dragones si tienen mas de 500 d energia)
+#cap 1,2 y 3 mumuki
+
+from aves import pepita, anastasia,roberta
+
+print(pepita.energia)
+
+print(pepita.esta_feliz())
+
+print(roberta)
+print(roberta.energia)
+print(roberta.esta_feliz())
