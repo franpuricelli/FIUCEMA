bb896a32342a95cb8172e52ddeed5e381ebc38a0
franpuricelli <francopurib@gmail.com>
2023-06-07 15:03:15 -0300

    clase 10 practica

---
+++ clase_10_practica/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+# python3 -m flask run #
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return render_template("home.html") #<p> etiqueta de parrafo
+
+
